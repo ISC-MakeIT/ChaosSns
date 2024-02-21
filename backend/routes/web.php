3bf04ee4c029f984b48bc737f90ecf58f2bd5b1b
sol <ocosio0907@gmail.com>
2024-02-21 13:01:09 +0900

    ルーティング設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('login');
+
+Route::get('/getTweets', [TweetController::class, 'getTweets']);
