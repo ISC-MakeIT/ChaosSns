4ede3075b48e84b7756f4f0e3568528083c9cc1b
sol <ocosio0907@gmail.com>
2024-02-21 13:21:03 +0900

    api.phpに移動してパス名変更

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\TweetController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,14 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('/users')->group(function() {
+Route::prefix('/users')->group(function () {
     Route::post('/', [UserController::class, 'create'])->name('users.registration');
     Route::post('/login', [UserController::class, 'login'])->name('users.login');
 });
 
-Route::middleware('auth:sanctum')->group(function() {
-    Route::prefix('/users')->group(function() {
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('/users')->group(function () {
         Route::get('/self', [UserController::class, 'getLoggedInUser'])->name('users.self');
         Route::post('/logout', [UserController::class, 'logout'])->name('users.logout');
     });
+});
+
+Route::prefix('/tweets')->group(function () {
+    Route::get('/', [TweetController::class, 'getTweets']);
 });
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('login');
-
-Route::get('/getTweets', [TweetController::class, 'getTweets']);
