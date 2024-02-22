0ef5fa989fb1f923f19c1725f206f22210435da0
tamagram <tamadevelopment211@gmail.com>
2024-02-22 18:52:33 +0900

    🐛 Set public path

--- frontend/src/consts/api.ts
+++ frontend/src/consts/api.ts
@@ -33,4 +33,4 @@ export const API_ROUTES: {[name: string]: ApiRoute} = {
     }
 } as const;
 
-export const API_BASE_URL = process.env.API_BASE_URL!;
+export const API_BASE_URL = process.env.API_BASE_URL || process.env.NEXT_PUBLIC_API_URL as string;
