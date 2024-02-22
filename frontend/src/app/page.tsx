bbe9e76ab9c31f60d7afcbb542617e44cc0673a3
tamagram <tamadevelopment211@gmail.com>
2024-02-22 22:30:05 +0900

    🚧 WIP

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,30 @@
+'use client'
+
 import Header from "@/components/organisms/Header";
+import { User, useUser } from "@/hooks/user";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [user, setUser] = useState<User | undefined>();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await useUser()
+        setUser(data)
+      } catch (e) {
+        console.log(e)
+      }
+    })()
+  }, [])
+
   return (
     <div>
       <Header>ホーム</Header>
+      <p className="text-white">
+        {user?.name ?? 'user name none'}
+        {user?.email ?? 'user email none'}
+      </p>
     </div>
   );
 }
