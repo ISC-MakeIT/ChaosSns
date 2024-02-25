32d4fa565337f7338696445956406507209b636f
八尋 諒 <yahiroryo@email.yappi.jp>
2024-02-25 10:41:30 +0900

    fix bash

--- k8s/bin/run.sh
+++ k8s/bin/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 main() {
-    docker build -t backend:latest -f ./docker/backend.Dockerfile ./../../docker
-    docker build -t frontend:latest -f ./docker/frontend.Dockerfile ./../../docker
-    docker build -t database:latest -f ./docker/database.Dockerfile ./../../docker
+    docker build -t backend:latest -f ./docker/backend.Dockerfile ./../../
+    docker build -t frontend:latest -f ./docker/frontend.Dockerfile ./../../
+    docker build -t database:latest -f ./docker/database.Dockerfile ./../../
     cd ..
     kubectl apply -f deployment.yaml
 }
